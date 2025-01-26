b6acd56191b3479c254b0a6035feccbdbea06a17
xhh4 <micheljaheim5@proton.me>
2025-01-25 22:35:55 -0500

    Update gun_data.lua

--- gun_data.lua
+++ gun_data.lua
@@ -1,1 +1,28 @@
+utility.get_gun_remote = function()
+   for _, v in pairs(game.ReplicatedStorage:GetDescendants()) do
+       if v.Name == "MainEvent" or v.Name == "Bullets" or v.Name == ".gg/untitledhood" or v.Name == "Remote" or v.Name == "MAINEVENT" or v.Name == ".gg/flamehood" then
+           return v
+       end
+   end
+end
 
+utility.get_gun_argument = function()
+   local placeid = game.PlaceId
+   if placeid == 2788229376 or placeid == 4106313503 or placeid == 11143225577 or placeid == 17319408836 or placeid == 18110728826 then
+       return "UpdateMousePosI"
+   elseif placeid == 5602055394 or placeid == 7951883376 then
+       return "MousePos" 
+   elseif placeid == 10100958808 or placeid == 12645617354 or placeid == 14171242539 or placeid == 14412436145 or placeid == 14412355918 or placeid == 14413720089 or placeid == 17403265390 or placeid == 17403166075 or placeid == 17403262882 or placeid == 15186202290 or placeid == 15763494605 then
+       return "MOUSE"
+   elseif placeid == 9825515356 then
+       return "MousePosUpdate"
+   elseif placeid == 15166543806 then
+       return "MoonUpdateMousePos"
+   elseif placeid == 16033173781 or placeid == 7213786345 then
+       return "UpdateMousePosI"
+   else
+       return "UpdateMousePos"
+   end
+end
+
+return utility
