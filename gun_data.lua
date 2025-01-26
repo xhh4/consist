e43f1029dfe0ef4cfb201d5353fca39377aecf44
xhh4 <micheljaheim5@proton.me>
2025-01-26 09:32:04 -0500

    Update gun_data.lua

--- gun_data.lua
+++ gun_data.lua
@@ -1,28 +1,51 @@
-return {
-    get_event = function()
-        for _, v in pairs(game.ReplicatedStorage:GetChildren()) do
-            if v.Name == "MainEvent" or v.Name == "Bullets" or v.Name == ".gg/untitledhood" or v.Name == "Remote" or v.Name == "MAINEVENT" or v.Name == ".gg/flamehood" then
-                return v
-            end
-        end
-    end,
-
-    get_args = function()
-        local place_id = game.PlaceId
-        if place_id == 2788229376 or place_id == 4106313503 or place_id == 11143225577 or place_id == 17319408836 or place_id == 18110728826 then
-            return "UpdateMousePosI"
-        elseif place_id == 5602055394 or place_id == 7951883376 then
-            return "MousePos"
-        elseif place_id == 10100958808 or place_id == 12645617354 or place_id == 14171242539 or place_id == 14412436145 or place_id == 14412355918 or place_id == 14413720089 or place_id == 17403265390 or place_id == 17403166075 or place_id == 17403262882 or place_id == 15186202290 or place_id == 15763494605 then
-            return "MOUSE"
-        elseif place_id == 9825515356 then
-            return "MousePosUpdate"
-        elseif place_id == 15166543806 then
-            return "MoonUpdateMousePos"
-        elseif place_id == 16033173781 or place_id == 7213786345 then
-            return "UpdateMousePosI"
-        else
-            return "UpdateMousePos"
-        end
-    end
+local gun_data = {
+   remote = {
+       names = {
+           "MainEvent",
+           "Bullets", 
+           ".gg/untitledhood",
+           "Remote",
+           "MAINEVENT",
+           ".gg/flamehood"
+       }
+   },
+   argument = {
+       default = "UpdateMousePos",
+       ["2788229376"] = "UpdateMousePosI", 
+       ["4106313503"] = "UpdateMousePosI",
+       ["11143225577"] = "UpdateMousePosI",
+       ["17319408836"] = "UpdateMousePosI",
+       ["18110728826"] = "UpdateMousePosI",
+       ["5602055394"] = "MousePos",
+       ["7951883376"] = "MousePos",
+       ["10100958808"] = "MOUSE",
+       ["12645617354"] = "MOUSE",
+       ["14171242539"] = "MOUSE",
+       ["14412436145"] = "MOUSE", 
+       ["14412355918"] = "MOUSE",
+       ["14413720089"] = "MOUSE",
+       ["17403265390"] = "MOUSE",
+       ["17403166075"] = "MOUSE",
+       ["17403262882"] = "MOUSE",
+       ["15186202290"] = "MOUSE",
+       ["15763494605"] = "MOUSE",
+       ["9825515356"] = "MousePosUpdate",
+       ["15166543806"] = "MoonUpdateMousePos",
+       ["16033173781"] = "UpdateMousePosI",
+       ["7213786345"] = "UpdateMousePosI"
+   }
 }
+
+
+gun_data.get_event = function()
+   for _, name in pairs(gun_data.remote.names) do
+       local remote = game.ReplicatedStorage:FindFirstChild(name)
+       if remote then return remote end
+   end
+end
+
+gun_data.get_args = function()
+   return gun_data.argument[tostring(game.PlaceId)] or gun_data.argument.default
+end
+
+return gun_data
